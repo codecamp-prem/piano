0e1ffb1cf4b2be14a6e519f7ee7347d358a95163
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-26 14:29:45 +0545

    instrument context provider

---
+++ src/state/instrument/Provider.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { DEFAULT_INSTRUMENT } from "../../domain/sound";
+import { InstrumentContext } from "./Context";
+
+export const InstrumentContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [instrument, setInstrument] = useState(DEFAULT_INSTRUMENT);
+
+  return (
+    <InstrumentContext.Provider value={{ instrument, setInstrument }}>
+      {children}
+    </InstrumentContext.Provider>
+  );
+};
