32ba67cc87f3bdbcc90d9cd3113b3613afd8930a
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-26 14:59:18 +0545

    getting all instrument provided by SoundFont library

--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,13 +1,19 @@
+import { useEffect } from "react";
 import { Keyboard } from ".";
 import { useSoundfont } from "../../adapters/Soundfont";
-import { useMount } from "../../utils/useMount/useMount";
+import { useInstrument } from "../../state/instrument";
+//import { useMount } from "../../utils/useMount/useMount";
 import { useAudioContext } from "../AudioContextProvider";
 
 export const KeyboardWithInstrument = () => {
   const AudioContext = useAudioContext()!;
-  const { loading, play, stop, load } = useSoundfont({ AudioContext });
+  const { instrument } = useInstrument();
+  const { loading, play, stop, load, current } = useSoundfont({ AudioContext });
 
-  useMount(() => load());
+  //useMount(() => load());
+  useEffect(() => {
+    if (!loading && instrument !== current) load(instrument);
+  }, [load, loading, instrument, current]);
 
   return <Keyboard loading={loading} play={play} stop={stop} />;
 };
